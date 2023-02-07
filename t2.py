3e6d0abf714a11e14c11a330e691069aa482951b
rakeshgiri1 <girirakesh8874@gmail.com>
2023-02-07 19:53:56 +0530

    design

---
+++ t2.py
@@ -0,0 +1,14 @@
+# pantacone.
+from turtle import *
+speed('slowest')
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+mainloop()
---
+++ t4.py
@@ -0,0 +1,12 @@
+from turtle import *
+side =6
+for i in range(side):
+    pencolor("blue")
+    fd(100)
+    lt(360/side)
+    pencolor('red')
+    dot(30)
+    for i in range (side):
+        fd(50)
+        lt(360/side) 
+mainloop()       
