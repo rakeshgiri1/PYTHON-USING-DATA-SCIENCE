fc332d6e58d668b71666b9fe1a22ad490642a19e
rakeshgiri1 <girirakesh8874@gmail.com>
2023-02-20 19:55:17 +0530

    STRING

--- game1.py
+++ game1.py
@@ -1,30 +1,36 @@
 import pgzrun
+WIDTH=1200
+HEIGHT=1600
 
 box=Rect((50,50),(150,100))
 box2=Rect((600,50),(100,100))
-#box3=Rect((300,50),(100,100))
-bvx =2
-b2vx=-2
+box3=Rect((300,50),(100,100))
+bvx =6
+b2vx=-6
+b3vx=-6
 
 def draw():
     screen.fill('blue')
     screen.draw.filled_rect(box,'yellow')
     screen.draw.filled_rect(box2,'red')
-   # screen.draw.filled_rect(box3,'green')
+    screen.draw.filled_rect(box3,'green')
 def update():
     global bvx,b2vx
-    box.x += bvx
+    box.x +=-bvx
     box2.x +=-b2vx
     if box.colliderect(box2):
         bvx=-bvx
-        b2vx=b2vx
+        b2vx=-b2vx
         sounds.cling.play()
     if box.left <0:
         bvx=-bvx
         sounds.cling.play()
     if box2.right>800:
         b2vx=-b2vx 
-        sounds.cling.play()    
+        sounds.cling.play()   
+    if box3.left>600:
+        b3vx=-b3vx 
+        sounds.cling.play()  
     #box.x +=2
     #box2.x +=-2
     #box3.y +=2
